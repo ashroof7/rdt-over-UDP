f2d7ed5cecdeffe397c6ee42d9e1abce6db21d20
leonardo7 <leo_aly7@ymail.com>
2013-11-17 17:57:30 +0200

    first run just communication between server and client.. contains bugs

--- src/client.c
+++ src/client.c
@@ -15,19 +15,21 @@
 #include <netdb.h>
 
 #define BUFFER_SIZE 1024
-#define PORT_NO 9999
+#define PORT_NO 7777
 #define HOSTNAME "localhost"
 
  char request_buffer[BUFFER_SIZE];
  unsigned char buffer[BUFFER_SIZE];
  int socket_fd;
+struct sockaddr_in server_addr;
+	socklen_t server_addr_len;
 
 
  char file_cname[100];
 // char read_buffer[BUFFER_SIZE];
 
  int parse_response() {
- 	int n = read(socket_fd, buffer, BUFFER_SIZE);
+ 	int n = recvfrom(socket_fd, buffer, BUFFER_SIZE, 0, (struct sockaddr*) &server_addr, &server_addr_len);
  //file size
  	FILE *op = fopen(file_cname, "wb");
 
@@ -42,9 +44,9 @@
 
  }
 
+ 	
  int main(int argc, char *argv[]) {
 
- 	struct sockaddr_in server_addr;
  	in_port_t port_no = PORT_NO;
  	struct hostent *server;
  	char *hostname = HOSTNAME;
@@ -66,18 +68,14 @@
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(port_no);
 	memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);	
-	
-	if (connect(socket_fd, (struct sockaddr*) &server_addr, sizeof(server_addr)) < 0) 
-			perror("ERROR couldn't connect to the server");
-	
 
  // TODO 
  // request_buffer = file_cname;
 	memcpy(&request_buffer, file_cname, sizeof(file_cname));
 
-
+	server_addr_len = sizeof(server_addr);
  	//send to server
- 	int n = send(socket_fd, request_buffer, strlen(request_buffer) , 0);
+ 	int n = sendto(socket_fd, request_buffer, strlen(request_buffer) , 0, (struct sockaddr*) &server_addr, server_addr_len);
  	if (n < 0)
  		perror("ERROR couldn't write to socket");
 
--- src/server.c
+++ src/server.c
@@ -12,7 +12,7 @@
 
 #define PKT_DATA_SIZE 1000 // identify data size (in bytes) in a packet
 #define TIME_OUT_VAL 500000ll // value in micro seconds
-#define PORT_NO 7777 
+#define PORT_NO 7777
 #define MAX_cwnd 30
 
 // TODO read from file 
@@ -22,14 +22,16 @@
 #define PKT_SIZE HEADER_SIZE+PKT_DATA_SIZE
 
 typedef struct{
- int16_t len;
- int16_t checksum;
- int32_t seqno;
- char data[PKT_DATA_SIZE];
+	int16_t len;
+	int16_t checksum;
+	int32_t seqno;
+	char data[PKT_DATA_SIZE];
 }pkt_t;
 
 int cwnd = MAX_cwnd;
 unsigned int trans_sock;
+
+int main_sock, worker_sock;
 
 
 pkt_t packet_buff[MAX_SEQ_N];
@@ -44,32 +46,35 @@ FILE *file;
 
 void read_nbyte(unsigned long offset, unsigned long n){
  // read n bytes from the the file starting from offset 
- fseek(file, offset, SEEK_SET);
- n = n>=MAX_SEQ_N? MAX_SEQ_N-1 : n;
- fread(file_buff, n, 1, file);
+	fseek(file, offset, SEEK_SET);
+	n = n>=MAX_SEQ_N? MAX_SEQ_N-1 : n;
+	fread(file_buff, n, 1, file);
 }
 
 unsigned long calc_file_size(){
  //Get file length
- fseek(file, 0, SEEK_END);
- unsigned long len=ftell(file);
- fseek(file, 0, SEEK_SET);
- return len;
+	fseek(file, 0, SEEK_END);
+	unsigned long len=ftell(file);
+	fseek(file, 0, SEEK_SET);
+	return len;
 }
 
 
 
 void process_pkt(int seqno){
- packet_buff[seqno].len = PKT_SIZE;
- packet_buff[seqno].seqno =  seqno ;
+	packet_buff[seqno].len = PKT_SIZE;
+	packet_buff[seqno].seqno =  seqno ;
 
- int offset = seqno*PKT_DATA_SIZE;
- memcpy(packet_buff[seqno].data, file_buff+offset, PKT_DATA_SIZE);
+	int offset = seqno*PKT_DATA_SIZE;
+	memcpy(packet_buff[seqno].data, file_buff+offset, PKT_DATA_SIZE);
 
  // TODO calc checksum or crc
  // pkt.checksum = ?
 
 }
+
+
+
 
 
 long long timer[MAX_cwnd];
@@ -78,13 +83,13 @@ int timer_n_index = cwnd; // largest index in the timers array
 
 void send_pkt(int seqno){
 	int nbytes = 0;
- if ( (nbytes = send(trans_sock, &packet_buff[seqno], PKT_SIZE, 0)) < 0){
-  perror("Sending packet");
- }else {
+	if ( (nbytes = send(trans_sock, &packet_buff[seqno], PKT_SIZE, 0)) < 0){
+		perror("Sending packet");
+	}else {
   //start timer 
-  valid_timer[seqno-buff_base] = 1;
-  timer[seqno-buff_base] = TIME_OUT_VAL;
- }
+		valid_timer[seqno-buff_base] = 1;
+		timer[seqno-buff_base] = TIME_OUT_VAL;
+	}
 
 }
 
@@ -98,22 +103,22 @@ unsigned char valid_w_timer[MAX_cwnd]; // boolean if timer is acive = true, fals
 // rename 
 void timer_handler(int sig) {
     //start in method timer
-    clock_t local_timer;
+	clock_t local_timer;
     long long min_timer = TIME_OUT_VAL; // set to maximum value
-  
+
     int i;
     for(i = 0; i <= timer_n_index; i++){
-        local_timer = clock(); 
-        if(valid_timer[i]){
-            request_timers[i] -= (welcome_timeval.tv_usec+welcome_timeval.tv_sec*1000000 
-             + ((float) local_timer)/CLOCKS_PER_SEC*1000000);
-            if (request_timers[i] <= 0){
+    	local_timer = clock(); 
+    	if(valid_timer[i]){
+    		request_timers[i] -= (welcome_timeval.tv_usec+welcome_timeval.tv_sec*1000000 
+    			+ ((float) local_timer)/CLOCKS_PER_SEC*1000000);
+    		if (request_timers[i] <= 0){
                 // fire_action(timer[i]);
-                request_timers[i] = TIME_OUT_VAL;
-            }else if (request_timers[i] < min_timer){
-                min_timer = request_timers[i];
-            }
-        }
+    			request_timers[i] = TIME_OUT_VAL;
+    		}else if (request_timers[i] < min_timer){
+    			min_timer = request_timers[i];
+    		}
+    	}
     }
 
     welcome_timeval.tv_sec = min_timer/1000000;
@@ -121,68 +126,89 @@ void timer_handler(int sig) {
 
     welcome_itimer.it_interval = welcome_timeval;
     welcome_itimer.it_value = welcome_timeval;
- setitimer(ITIMER_REAL, &welcome_itimer, NULL);
+    setitimer(ITIMER_REAL, &welcome_itimer, NULL);
 }
 
+
+
+
+
+
+#define RQST_BUFF_SZ 100
+// assuming that the coming request will always fit in this buffer
+char request_buff[RQST_BUFF_SZ]; 
+void receive_rqst(){
+
+
+}
+
+void rdt(){
+ // receive request
+ // since we know the server the request message only contains the file path/name
+ recv(worker_sock, request_buff, RQST_BUFF_SZ, 0); //here blocking receiving 
+ printf("%s\n", request_buff);
+ // ACK request
+
+ // send data
+}
 
 
 int main(){
 
 
-    signal(SIGALRM, timer_handler);
+	signal(SIGALRM, timer_handler);
 
 
- int main_sock, worker_sock;
- in_port_t port_no = PORT_NO;
- struct sockaddr_in server_addr, client_addr;
+	in_port_t port_no = PORT_NO;
+	struct sockaddr_in server_addr, client_addr;
 
 
 
- if(main_sock = socket(AF_INET, SOCK_DGRAM, 0) < 0){
-  perror("Error: Creating the main socket");
-  return EXIT_FAILURE;
- }
+	if( (main_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+		perror("Error: Creating the main socket");
+		return EXIT_FAILURE;
+	}
 
 
- memset((char *)&server_addr, 0, sizeof(server_addr));// initialization
- // 0 init is important since the server_addr.zero[] must be zero
- server_addr.sin_addr.s_addr = INADDR_ANY;
- server_addr.sin_family = AF_INET;
- server_addr.sin_port = htons(port_no);
+	 memset((char *)&server_addr, 0, sizeof(server_addr));// initialization
+	 // 0 init is important since the server_addr.zero[] must be zero
+	 server_addr.sin_family = AF_INET;
+	 server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	 server_addr.sin_port = htons(port_no);
 
- if (bind(main_sock, (struct sockaddr*) &server_addr, sizeof(server_addr))  < 0){
-  perror("ERROR on binding socket to server address");
-  return EXIT_FAILURE;
+ if ( (bind(main_sock, (struct sockaddr*) &server_addr, sizeof(server_addr)))  < 0){
+ 	perror("ERROR on binding socket to server address");
+ 	return EXIT_FAILURE;
  }
 
  int worker_pid;
- socklen_t client_addr_len = sizeof(client_addr);
+ socklen_t server_addr_len = sizeof(server_addr);
 
  while (1) {
-  listen(main_sock, 5);
-  worker_sock = accept(main_sock, (struct sockaddr*) &client_addr, &client_addr_len);
+ 	int	recvlen = recvfrom(main_sock, request_buff, (size_t)RQST_BUFF_SZ, 0, (struct sockaddr*) &server_addr, &server_addr_len);
 
-  if (worker_sock < 0) {
-   perror("ERROR accepting the new connection");
-   continue;
-  }
-
-  if (worker_pid = fork() < 0) {
-   perror("ERROR on forking a new worker process");
-   continue;
-  }
+ 	if ( worker_sock < 0) {
+ 		perror("ERROR accepting the new connection");
+ 		continue;
+ 	}
+ 	printf("%s\n",request_buff);
+ 	worker_pid = fork();
+ 	if (worker_pid = fork() < 0) {
+ 		perror("ERROR on forking a new worker process");
+ 		continue;
+ 	}
 
   if (!worker_pid) { // worker_pid = 0 then we are in the child
-      signal(SIGALRM, timer_handler);
+  	signal(SIGALRM, timer_handler);
 
-   close(main_sock);
-   exit(EXIT_SUCCESS);
+  	close(main_sock);
+  	exit(EXIT_SUCCESS);
   } else { // pid != 0 then we are in the parent;
-   close(worker_sock);
+  	close(worker_sock);
   }
 
- }
- return EXIT_SUCCESS;
+}
+return EXIT_SUCCESS;
 
 
 
